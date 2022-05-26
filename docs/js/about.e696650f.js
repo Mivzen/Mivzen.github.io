475489223dccc862da6d4878cd16013bc2981ae7
Søren Lund <lund14@gmail.com>
2022-05-26 16:09:22 +0200

    Build the project

---
+++ docs/js/about.e696650f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmivzen_github_io"]=self["webpackChunkmivzen_github_io"]||[]).push([[443],{7381:function(n,u,i){i.r(u),i.d(u,{default:function(){return b}});var t=i(3396);const e={class:"about"},a=(0,t._)("h1",null,"This is an about page",-1),r=[a];function s(n,u){return(0,t.wg)(),(0,t.iD)("div",e,r)}var c=i(89);const o={},h=(0,c.Z)(o,[["render",s]]);var b=h}}]);
+//# sourceMappingURL=about.e696650f.js.map
